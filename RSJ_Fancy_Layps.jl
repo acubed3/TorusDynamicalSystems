8f97997c8d87ee1c1864323c6c445acad17ee9c8
acubed3 <aleksandrov.aa@phystech.edu>
2025-01-09 14:21:45 +0300

    fixed major error

--- RSJ_Fancy_Layps.jl
+++ RSJ_Fancy_Layps.jl
@@ -66,7 +66,7 @@ function phase_lock_areas!(omega)
 	name_pattern = replace(name_pattern, "." => "_")
 	name = join([name_pattern, ".csv"])
 
-	writedlm(name,  wm_values, ',')
+	writedlm(name,  layp_exps, ',')
 	
 end
 
