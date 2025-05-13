cfa1333fba56e9161f5876f892d9f5382102c755
acubed3 <aleksandrov.aa@phystech.edu>
2025-05-13 20:22:21 +0300

    many bugs fixed

--- RSJ_Noise.jl
+++ RSJ_Noise.jl
@@ -3,65 +3,73 @@ using Distributions
 using DelimitedFiles
 using ProgressMeter
 
-function phase_lock_areas!(D)
+function phase_lock_areas!(omega, D, A_step)
 
-	function RSJ!(dtheta, theta, p, t)
+	@show(omega, D, A_step)
+
+	function RSJ!(du, u, p, t)
 		omega, A, B, D = p
-		dtheta[1] = -sin(theta[1]) + B + A*cos(omega*theta[2])
-		dtheta[2] = 1
+		du[1] = -1/2*(conj(u[2])-u[2]*u[1]^2)+1im*(B+A*cos(omega*t))*u[1]
+		du[2] = -1/2*(1-abs(u[2])^2)*conj(u[1])
 		nothing
 	end
 
-	function noise!(dtheta, theta, p, t)
+	function noise!(du, u, p, t)
 		omega, A, B, D = p
-		dtheta[1] = sqrt(2*D)
-		dtheta[2]
+		du[1] = sqrt(2*D)
+		du[2] = 0
 		nothing
 	end
 	
-	function winding_number!(omega, A, B, D, t0)
-		theta_0 = [2*pi*rand(),1]
+	function lyapunov!(omega, A, B, D)
+		t0 = 2*pi/omega
+		zeta_w = [1+1im*0.0, 0+1im*0.0]
 		tspan = (0., t0)
 		
 		p = (omega, A, B, D)
-		prob = SDEProblem(RSJ!, noise!, theta_0, tspan, p)
+		prob = SDEProblem(RSJ!, noise!, zeta_w, tspan, p)
 		sol = solve(prob)
+		zeta = last(sol.u)[1]
+		w = last(sol.u)[2]
 		
-		phase = last(sol.u)[1]
-		wm = phase/(omega*t0)
-		return wm
+		numerator = (1+zeta+sqrt((1-zeta)^2+4*zeta*abs(w)^2))
+		denominator = (1+zeta-sqrt((1-zeta)^2+4*zeta*abs(w)^2))
+		lyap = abs(numerator/denominator)
+		
+		return lyap
 	end
 
-	A_min = -2.0;
-	A_max = +8.0;
-	A_step = 0.01;
+	A_min = 0.0;
+	A_max = 4.0;
 
 	A_values = range(A_min, A_max, step=A_step) |> collect;
 
-	B_min = -2.0;
-	B_max = 2.0;
-	B_step = 0.01;
+	B_min = 0.0;
+	B_max = 4.0;
 
-	B_values = range(B_min, B_max, step=B_step) |> collect;
-
-	omega = 1.0;
-	t0 = 1000;
+	B_values = range(B_min, B_max, step=A_step) |> collect;
 
 	A_size = length(A_values);
 	B_size = length(B_values);
 
-	wm_values = zeros(B_size, A_size);
+	layp_values = zeros(B_size, A_size);
 
 	@showprogress Threads.@threads for i=1:B_size  
 		for j=1:A_size
-			wm_values[i,j] = winding_number!(omega, A_values[j], B_values[i], D, t0)
+			layp_values[i,j] = lyapunov!(omega, A_values[j], B_values[i], D)
 		end
 	end
 
-	name_pattern = join(["ph_lock_D_", string(D)])
+	name_pattern = join(["ph_lock_D_", string(D), "_omega_", string(omega), "_step_", string(A_step)])
 	name_pattern = replace(name_pattern, "." => "_")
 	name = join([name_pattern, ".csv"])
 
-	writedlm(name,  wm_values, ',')
+	writedlm(name,  layp_values, ',')
 	
 end
+
+omega = parse(Float64, ARGS[1])
+D = parse(Float64, ARGS[2])
+A_step = parse(Float64, ARGS[3])
+phase_lock_areas!(omega, D, A_step)
+
